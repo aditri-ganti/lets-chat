e1a45b4eae1b3ea19615c86f8fa89bbf3d7d42cc
aditri-ganti <79452630+aditri-ganti@users.noreply.github.com>
2022-03-17 10:57:47 +0530

    Add files via upload

---
+++ Kwitte_room.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD7rJxirgRD42b5ves55ec9xZHRHhyNtEI",
+    authDomain: "lets-chat-850f9.firebaseapp.com",
+    projectId: "lets-chat-850f9",
+    storageBucket: "lets-chat-850f9.appspot.com",
+    messagingSenderId: "992620710840",
+    appId: "1:992620710840:web:1436733896efbc0fa39d4d"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  
